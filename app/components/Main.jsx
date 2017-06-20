6985bf7402e741cd2179f5f1cdfacddcde2c186f
Matthew Rice <matthewrice11@yahoo.com>
2017-06-19 21:08:17 -0700

    Add Countdown Form and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,12 +5,10 @@ var Timer = require('Timer');
 var Main = (props) => {
     return (
         <div>
-            <div>
-                <div>
-                  <Nav/>
-
-                    <p>Main.jsx Rendered</p>
-                    {props.children}
+          <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
+                  {props.children}
                 </div>
 
             </div>
